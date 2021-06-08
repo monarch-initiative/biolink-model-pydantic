38a5eb063a45faf165a7257f4f94d35b7a5ea541
kshefchek <kshefchek@gmail.com>
2021-06-08 10:54:44 -0400

    add docs, curie tests

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # biolink-model-pydantic
 Pydantic dataclasses for the Biolink model
+
+
+#### Installation
+
+```
+pip install biolink_model_pydantic
+```
+
+#### Using the dataclasses
+
+```python
+from biolink_model_pydantic.model import Gene
+
+gen_entity = Gene(id='NCBIGene:123', in_taxon='NCBITaxon:9606')
+```
---
+++ tests/test_curie_regex.py
@@ -0,0 +1,31 @@
+import re
+
+import pytest
+
+curie_regexp = re.compile(r'^[a-zA-Z_]?[a-zA-Z_0-9-]*:([A-Za-z0-9_][A-Za-z0-9_.-]*[A-Za-z0-9_]*)?$')
+
+
+@pytest.mark.parametrize(
+    "curie",
+    [
+        "foo:bar",
+        "foo:123",
+        "foo:",
+        "123:456",
+        "1-2:3-4"
+    ],
+)
+def test_valid_curie(curie):
+    assert re.match(curie_regexp, curie)
+
+
+@pytest.mark.parametrize(
+    "curie",
+    [
+        "http://123",
+        "://",
+        "NotACurie"
+    ],
+)
+def test_invalid_curie(curie):
+    assert not re.match(curie_regexp, curie)
