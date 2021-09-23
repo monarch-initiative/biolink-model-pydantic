f2ab03eb3d799e0211fb0c073911abae7568167c
Kevin Schaper <kevinschaper@gmail.com>
2021-09-23 12:10:56 -0700

    updated release to 0.1.0

--- biolink_model_pydantic/__init__.py
+++ biolink_model_pydantic/__init__.py
@@ -1,2 +1,2 @@
 """Pydantic dataclasses for the Biolink model"""
-__version__ = '0.0.7'
+__version__ = '0.1.0'
--- biolink_model_pydantic/model.py
+++ biolink_model_pydantic/model.py
@@ -1,5 +1,5 @@
 # Auto generated from biolink-model.yaml by pydanticgen.py version: 0.9.0
-# Generation date: 2021-09-23 12:02
+# Generation date: 2021-09-23 12:10
 # Schema: Biolink-Model
 #
 # id: https://w3id.org/biolink/biolink-model
