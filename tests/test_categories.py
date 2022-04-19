16cc73e1da4ae675f87e9d23678a3df091a563d0
Kevin Schaper <kevinschaper@gmail.com>
2022-04-19 10:09:39 -0700

    Simplified category name prepopulation to just have the direct class, rather than a full closure

--- tests/test_categories.py
+++ tests/test_categories.py
@@ -4,13 +4,9 @@ Testing the biolink model dataclasses + pydandic
 from biolink_model_pydantic.model import MolecularEntity
 
 
-def test_default_categories():
+def test_default_category():
     """
     Test that categories are inferred from the mro chain
     """
     molec_entity = MolecularEntity(id='HP:123', type='SO:123')
-    assert set(molec_entity.category) == {
-        'biolink:NamedThing',
-        'biolink:ChemicalEntity',
-        'biolink:MolecularEntity',
-    }
+    assert set(molec_entity.category) == {'biolink:MolecularEntity'}
